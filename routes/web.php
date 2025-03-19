cfd92e515c7beca7785ce7f9f8ba2057ff6025a0
wndygrng1811 <windyyohana242@gmail.com>
2025-03-19 11:25:41 +0700

    forsit commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+//    return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID' . $id;
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function() {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function() {
+        return 'Admin Users';
+    });
+});
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/login', [AuthController::class, 'index'])->name('login');
